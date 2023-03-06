e7b1f53b5e5ba62bc2c1681c72205fecf24ae141
Vidmantas <valskisv@gmail.com>
2023-03-06 21:12:07 +0000

    update web routes and cat controller

--- src/web-routes.js
+++ src/web-routes.js
@@ -1,5 +1,7 @@
+import { aboutController } from "./controllers/about-controller.js";
 import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { categoryController } from "./controllers/category-controller.js";
 
 export const webRoutes = [
   { method: "GET", path: "/", config: accountsController.index },
@@ -9,6 +11,13 @@ export const webRoutes = [
   { method: "POST", path: "/register", config: accountsController.signup },
   { method: "POST", path: "/authenticate", config: accountsController.login },
 
+  { method: "GET", path: "/about", config: aboutController.index },
+
   { method: "GET", path: "/dashboard", config: dashboardController.index },
   { method: "POST", path: "/dashboard/addcategory", config: dashboardController.addCategory },
+  { method: "GET", path: "/dashboard/deletecategory/{id}", config: dashboardController.deleteCategory },
+
+  { method: "GET", path: "/category/{id}", config: categoryController.index },
+  { method: "POST", path: "/category/{id}/addtrack", config: categoryController.addTrack },
+  { method: "GET", path: "/category/{id}/deletetrack/{trackid}", config: categoryController.deleteTrack },
 ];
