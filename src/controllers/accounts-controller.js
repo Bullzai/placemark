2afcbcaa48b6bf1ada513e94072006cb21391d22
Vidmantas <valskisv@gmail.com>
2023-03-07 19:46:11 +0000

    fix cookie error preventing for loading the web

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,13 +1,15 @@
+import { UserSpec, UserCredentialsSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
-import { UserSpec, } from "../models/joi-schemas.js";
 
 export const accountsController = {
   index: {
+    auth: false,
     handler: function (request, h) {
       return h.view("main", { title: "Welcome to Placemark" });
     },
   },
   showSignup: {
+    auth: false,
     handler: function (request, h) {
       return h.view("signup-view", { title: "Sign up for Placemark" });
     },
@@ -18,7 +20,7 @@ export const accountsController = {
       payload: UserSpec,
       options: { abortEarly: false },
       failAction: function (request, h, error) {
-        return h.view("signup-view", { title: "Sign up error" }).takeover().code(400);
+        return h.view("signup-view", { title: "Sign up error", errors: error.details }).takeover().code(400);
       },
     },
     handler: async function (request, h) {
@@ -28,23 +30,42 @@ export const accountsController = {
     },
   },
   showLogin: {
+    auth: false,
     handler: function (request, h) {
       return h.view("login-view", { title: "Login to Placemark" });
     },
   },
   login: {
+    auth: false,
+    validate: {
+      payload: UserCredentialsSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("login-view", { title: "Log in error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
       if (!user || user.password !== password) {
         return h.redirect("/");
       }
+      request.cookieAuth.set({ id: user._id });
       return h.redirect("/dashboard");
     },
   },
   logout: {
     handler: function (request, h) {
+      request.cookieAuth.clear();
       return h.redirect("/");
     },
+  },
+
+  async validate(request, session) {
+    const user = await db.userStore.getUserById(session.id);
+    if (!user) {
+      return { isValid: false };
+    }
+    return { isValid: true, credentials: user };
   },
 };
