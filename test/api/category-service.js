b1fce4d3914a3b5187173d7680a6e0c04ae3527d
Vidmantas <valskisv@gmail.com>
2023-03-08 22:09:20 +0000

    added placemark API and their tests

--- test/api/category-service.js
+++ test/api/category-service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-import { serviceUrl } from "../fixtures.js";
+import { maggie, serviceUrl } from "../fixtures.js";
 
 export const categoryService = {
   categoryUrl: serviceUrl,
@@ -16,8 +15,12 @@ export const categoryService = {
   },
 
   async getAllUsers() {
-    const res = await axios.get(`${this.categoryUrl}/api/users`);
-    return res.data;
+    try {
+      const res = await axios.get(`${this.categoryUrl}/api/users`);
+      return res.data;
+    } catch (e) {
+      return null;
+    }
   },
 
   async deleteAllUsers() {
@@ -49,4 +52,39 @@ export const categoryService = {
     const res = await axios.get(`${this.categoryUrl}/api/categories/${id}`);
     return res.data;
   },
+
+  async getAllPlacemarks() {
+    const res = await axios.get(`${this.categoryUrl}/api/placemarks`);
+    return res.data;
+  },
+
+  async createPlacemark(id, placemark) {
+    const res = await axios.post(`${this.categoryUrl}/api/categories/${id}/placemarks`, placemark);
+    return res.data;
+  },
+
+  async deleteAllPlacemarks() {
+    const res = await axios.delete(`${this.categoryUrl}/api/placemarks`);
+    return res.data;
+  },
+
+  async getPlacemark(id) {
+    const res = await axios.get(`${this.categoryUrl}/api/placemarks/${id}`);
+    return res.data;
+  },
+
+  async deletePlacemark(id) {
+    const res = await axios.delete(`${this.categoryUrl}/api/placemarks/${id}`);
+    return res.data;
+  },
+
+  // async authenticate(user) {
+  //   const response = await axios.post(`${this.categoryUrl}/api/users/authenticate`, user);
+  //   axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.token;
+  //   return response.data;
+  // },
+
+  // async clearAuth() {
+  //   axios.defaults.headers.common["Authorization"] = "";
+  // },
 };
