76cf50153572f0c3143c0613e5ec401135fe3a55
Vidmantas <valskisv@gmail.com>
2023-03-18 18:20:44 +0000

    fixed user firebase store to work properly

--- src/models/firebase/user-firebase-store.js
+++ src/models/firebase/user-firebase-store.js
@@ -1,20 +1,7 @@
-// import { ref, push, remove, update, child, orderByChild, equalTo, get, set } from "firebase/database";
 import { getDatabase, ref, set, push, get, child, update, remove, query, orderByChild, equalTo } from "firebase/database";
-import { Query } from "mongoose";
 import database from "./firebase-config.js";
 
 const usersRef = ref(database, "users");
-
-// function writeUserData(name, email) {
-//   // const db = getDatabase();ref(database, "users/" + userId)
-//   const newUserRef = push(usersRef);
-//   set(newUserRef, {
-//     username: name,
-//     email: email,
-//   });
-// }
-
-// writeUserData("vid", "vava@gmail.com");
 
 export const userFirebaseStore = {
   async getAllUsers() {
@@ -37,46 +24,46 @@ export const userFirebaseStore = {
     return null;
   },
 
-  async getUserByEmail(email) {
-    const emailQuery = query(usersRef, orderByChild("email"), equalTo(email));
-
-    // Snippet from Firebase docs that actually will tell you what rule you need to add.
-    // get(query(usersRef, orderByChild("email"), equalTo(email))).then((snapshot) => {
-    //   if (snapshot.exists()) {
-    //     console.log(snapshot.val());
-    //   } else {
-    //     console.log("No data available");
-    //   }
-    // }).catch((error) => {
-    //   console.error(error);
-    // });
-
-    const snapshot = await get(emailQuery);
-
-    if (snapshot.exists()) {
-      const userKey = Object.keys(snapshot.val())[0];
-      const userData = snapshot.val()[userKey];
-      return { _id: userKey, ...userData };
-    }
-    return null;
-  },
-
   async addUser(user) {
-    const obj = JSON.parse(JSON.stringify(user));
+    // Thanks to https://stackoverflow.com/questions/56298481/how-to-fix-object-null-prototype-title-product
+    const fixedUser = JSON.parse(JSON.stringify(user))
     const newUserRef = push(usersRef);
-    await set(newUserRef, obj);
+    await set(newUserRef, fixedUser);
     const newUserSnapshot = await get(newUserRef);
     const newUser = newUserSnapshot.val();
     return { _id: newUserRef.key, ...newUser };
   },
 
-  async editUser(user) {
-    const userRef = child(usersRef, user._id);
-    await update(userRef, user);
+  async getUserByEmail(email) {
+    const emailQuery = query(usersRef, orderByChild("email"), equalTo(email));
+    const snapshot = await get(emailQuery);
+    const result = [];
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+      result.push({ _id: childKey, ...childData });
+    });
+    return result.length ? result[0] : null;
   },
 
   async deleteUserById(id) {
-    const userRef = child(usersRef, id);
-    await remove(userRef);
+    await remove(child(usersRef, id));
+  },
+
+  async deleteAll() {
+    await set(usersRef, {});
+  },
+
+  async editUser(user) {
+    // Thanks to https://stackoverflow.com/questions/56298481/how-to-fix-object-null-prototype-title-product
+    const fixedUser = JSON.parse(JSON.stringify(user))
+    const userId = fixedUser._id;
+    // Don't update the _id.
+    delete fixedUser._id;
+
+    // Update the user
+    const userRef = child(usersRef, userId);
+
+    await update(userRef, fixedUser);
   },
 };
