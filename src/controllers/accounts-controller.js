e03551153cf76549694c080018083c3554487536
Vidmantas <valskisv@gmail.com>
2023-05-01 13:12:37 +0100

    implement password hashing and salting with bcrypt

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,5 +1,8 @@
+import bcrypt from "bcrypt";
 import { UserSpec, UserCredentialsSpec, UserEditSpec, AdminSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
+
+const saltRounds = 10;  // "cost factor" than controls the time taken to calculate the hash
 
 export const accountsController = {
   index: {
@@ -25,6 +28,7 @@ export const accountsController = {
     },
     handler: async function (request, h) {
       const user = request.payload;
+      user.password = await bcrypt.hash(user.password, saltRounds); // hash & salt the password
       await db.userStore.addUser(user);
       return h.redirect("/");
     },
@@ -47,7 +51,8 @@ export const accountsController = {
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
-      if (!user || user.password !== password) {
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (!user || !passwordsMatch) {
         return h.redirect("/");
       }
       request.cookieAuth.set({ id: user._id });
